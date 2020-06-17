a2f0b860453baa73efd43b90e898365fdc2ad9e1
Matheus Figueiredo <matheus.figueiredo@nave.rs>
2020-06-16 21:12:22 -0300

    feat: [Componente] Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,66 +5,79 @@ import PropTypes from 'prop-types'
 
 import { Typography } from '../'
 
-const CustomButton = forwardRef(({ type, variant, children, color, fontSize, fontWeight, ...props }, ref) => {
+const CustomButton = forwardRef(({ type, variant, children, color, fontSize, disabled, fontWeight, ...props }, ref) => {
+  const nameColor = color.split('.') // Apenas para mostrar a variação de cores
   return (
-    <Button type={type} color={color} ref={ref} variant={variant} {...props}>
-      <Typography color={color} fontSize={fontSize} fontWeight={fontWeight}>
+    <Button
+      type={type}
+      color={color}
+      colorVariant={nameColor}
+      ref={ref}
+      variant={disabled ? 'disabled' : variant}
+      disabled={disabled}
+      {...props}
+    >
+      <Typography
+        color={variant === 'filled' || disabled ? 'white' : color}
+        fontSize={fontSize}
+        fontWeight={fontWeight}
+      >
         {children}
       </Typography>
     </Button>
   )
 })
 
-const Button = styled('button')(
-  {
-    width: '100%',
-    border: 0,
-    borderRadius: 4,
-    transition: 'all .5s ease-in-out',
-    '&:hover': {
-      cursor: 'pointer'
-    }
-  },
-  variant({
-    prop: 'variant',
-    variants: {
-      filled: {
-        backgroundColor: 'blue.500',
-        '&:hover': {
-          bg: 'blue.600'
+const Button = styled.button`
+  width: 100%;
+  border-width: 2px;
+  border-radius: 4px;
+  border-style: solid;
+  box-sizing: border-box;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+  }
+  ${({ color, colorVariant }) =>
+    variant({
+      variants: {
+        filled: {
+          backgroundColor: color,
+          borderColor: color,
+          '&:hover': {
+            backgroundColor: `${colorVariant[0]}.600`,
+            borderColor: `${colorVariant[0]}.600`
+          },
+          '&:active': {
+            backgroundColor: `${colorVariant[0]}.800`,
+            borderColor: `${colorVariant[0]}.800`
+          }
         },
-        '&:active': {
-          bg: 'blue.800'
-        }
-      },
-      outline: {
-        border: '1px solid blue',
-        bg: 'white',
-        '&:hover': {
-          borderColor: 'blue.600'
+        outline: {
+          backgroundColor: 'transparent',
+          borderColor: color,
+          '&:hover': {
+            borderColor: `${colorVariant[0]}.600`
+          },
+          '&:active': {
+            borderColor: `${colorVariant[0]}.800`
+          }
         },
-        '&:active': {
-          borderColor: 'blue.800'
-        }
-      },
-      text: {
-        borderColor: 'transparent',
-        backgroundColor: 'transparent'
-      },
-      disabled: {
-        backgroundColor: 'gray.500',
-        '&:hover': {
-          cursor: 'not-allowed'
+        text: {
+          backgroundColor: 'transparent',
+          borderColor: 'transparent'
+        },
+        disabled: {
+          backgroundColor: 'gray.500',
+          borderColor: 'gray.500'
         }
       }
-    }
-  })
-)
-
+    })}
+`
 Button.defaultProps = {
   type: 'submit',
   variant: 'filled',
-  color: 'white'
+  color: 'gray.500'
 }
 
 Button.propTypes = {
