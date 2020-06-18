8ecebdd473eeace34d30f7a80ca8304adb61c834
Eduardo Bittencourt <eduardo.bittencourt@nave.rs>
2020-06-18 18:31:03 -0300

    feat(tag): add main tag structure

---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { color, layout, space, border } from 'styled-system'
+import { Typography } from '../'
+
+const Tag = ({ children, variant, fontWeight, color, ...props }) => {
+  return (
+    <Base display='inline-block' p={2} m={2} borderRadius={2} {...props}>
+      <Typography fontSize={`tag.${variant}`} fontWeight={fontWeight} color={color}>
+        {children}
+      </Typography>
+    </Base>
+  )
+}
+
+const Base = styled.div`
+  ${color}
+  ${layout}
+  ${space}
+  ${border}
+  cursor: pointer;
+`
+
+Tag.defaultProps = {
+  variant: 'md',
+  fontWeight: 'tag',
+  backgroundColor: 'red.main',
+  color: 'white'
+}
+
+Tag.propTypes = {
+  variant: PropTypes.oneOf(['sm', 'md'])
+}
+
+export default Tag
