1ec267ab283a0e10bf30db99a3b4ef57d5bd7572
Eduardo Bittencourt <eduardo.bittencourt@nave.rs>
2020-06-20 19:35:13 -0300

    feat(iconography): add first structure && add all icons

---
+++ src/components/Iconography/Icon.jsx
@@ -0,0 +1,24 @@
+import React, { useMemo } from 'react'
+import PropTypes from 'prop-types'
+
+const Icon = ({ icon }) => {
+  const src = useMemo(() => {
+    try {
+      return require(`../../assets/icons/${icon}.svg`)
+    } catch {
+      return require('../../assets/icons/clear.svg')
+    }
+  }, [icon])
+
+  return <img src={src} />
+}
+
+Icon.defaultProps = {
+  icon: 'build'
+}
+
+Icon.propTypes = {
+  icon: PropTypes.string
+}
+
+export default Icon
