d801055b36a23f50bf892ca75bb3badf7f9407c4
Eduardo Bittencourt <eduardo.bittencourt@nave.rs>
2020-06-22 10:01:36 -0300

    style(storybook): change storybook theme

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'Nave Kit',
+  brandUrl: 'https://nave.rs',
+  brandImage: 'https://nave-site-staging.herokuapp.com/static/59ee89e04b0e6b39dbc6c3a017dc62fa/3d1ac/logo-black.png',
+})
