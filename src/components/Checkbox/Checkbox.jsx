978bc0f9b4a7498bf9c6555845a50ec7b1adc2e8
Thiago Nunes Batista <thiago.batista@nave.rs>
2020-08-10 09:59:04 -0300

    feat(checkbox): add forward ref and name prop

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import React, { useState } from 'react'
+import React, { forwardRef, useState } from 'react'
 import styled, { css, down, typography, variant } from '@xstyled/styled-components'
 
 import { Typography } from '../'
@@ -8,35 +8,40 @@ import { Icon } from '../Iconography'
 const getIcon = checked => (checked ? 'checkbox_checked' : 'checkbox_outline')
 const getVariant = (checked, disabled) => (disabled ? 'disabled' : checked ? 'checked' : 'base')
 
-const Checkbox = ({ checked: propsChecked, color, disabled, onChange = () => {}, label, ...props }) => {
-  const [checked, setChecked] = useState(propsChecked)
+const Checkbox = forwardRef(
+  ({ checked: propsChecked, color, disabled, onChange = () => {}, label, name, ref, ...props }) => {
+    const [checked, setChecked] = useState(propsChecked)
 
-  return (
-    <LabelContainer color={color} variant={getVariant(checked, disabled)}>
-      <Icon icon={getIcon(checked)} height='24' />
-      <Typography fontSize={3} lineHeight={3} fontWeight={0} marginLeft={3}>
-        {label}
-      </Typography>
-      <Input
-        type='checkbox'
-        onChange={e => {
-          setChecked(e.target.checked)
-          onChange()
-        }}
-        defaultChecked={propsChecked}
-        disabled={disabled}
-        {...props}
-      />
-    </LabelContainer>
-  )
-}
+    return (
+      <LabelContainer color={color} variant={getVariant(checked, disabled)}>
+        <Icon icon={getIcon(checked)} height='24' />
+        <Typography fontSize={3} lineHeight={3} fontWeight={0} marginLeft={3}>
+          {label}
+        </Typography>
+        <Input
+          type='checkbox'
+          onChange={e => {
+            setChecked(e.target.checked)
+            onChange()
+          }}
+          defaultChecked={propsChecked}
+          disabled={disabled}
+          name={name}
+          ref={ref}
+          {...props}
+        />
+      </LabelContainer>
+    )
+  }
+)
 
 Checkbox.propTypes = {
   checked: PropTypes.bool,
   color: PropTypes.string,
   disabled: PropTypes.bool,
   onChange: PropTypes.func,
-  label: PropTypes.string
+  label: PropTypes.string,
+  name: PropTypes.string
 }
 
 Checkbox.defaultProps = {
