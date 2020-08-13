29c898670fc0eb8eacefba14da8a36a3518ed797
Thiago Nunes Batista <thiago.batista@nave.rs>
2020-08-13 11:16:09 -0300

    feat(loader): add initial work

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,25 +1,62 @@
+import React, { useEffect } from 'react'
+import styled, { css, variant } from '@xstyled/styled-components'
 import PropTypes from 'prop-types'
-import React from 'react'
-import styled, { borders, layout } from '@xstyled/styled-components'
 
-import { Typography } from '../'
+/*
 
-const getSize = size => (size === 'big' ? 76.8 : 16)
 
-const getBorderWidth = size => (size === 'big' ? 19.2 : 8)
+variant (pode receber icon ou major)
 
-export const Loader = ({ percentage, showPercentage, size, variant }) => {
+size (pode receber small ou big, utilizado para controlar o tamanho do loader caso sua variant seja icon)
+
+percentage (recebe um número inteiro de 0 a 100. caso não receba nenhum valor em percentage, apresentar variação indeterminate)
+
+showPercentage (recebe um boolean que controla se a porcentagem deve ou não ser mostrada no loader)
+
+
+
+Definição das variações no Storybook: As seguintes variantes do componente devem estar documentadas no Storybook
+
+Base (apresentar o componente sem nenhuma alteração de prop)
+
+Variant (apresentar as variações icon e major)
+
+Size (apresentar as variações de size)
+
+Percentage (apresentar as variações determinate do componente, com e sem o percentual no centro do loader
+*/
+
+const setIntervalPercentage = percentage => {
+  let current = 0
+  const counter = document.getElementById('counter')
+  counter.innerText = '0 %'
+
+  setInterval(() => {
+    if (current <= percentage) {
+      current++
+      const value = ((100 * current) / percentage).toFixed(0)
+      counter.innerText = `${value} %`
+    } else {
+      return
+    }
+  }, 1000)
+}
+
+export const Loader = ({ percentage = 5, showPercentage, size, variant }) => {
+  // useEffect(() => {
+  //   setIntervalPercentage(percentage)
+  // }, [])
+
   return (
-    <StyledLoaderSpinner>
-      <Spinner size={getSize(size)} borderWidth={getBorderWidth(size)} height={getSize(size)} width={getSize(size)} />
-      {showPercentage ? (
-        <Typography fontSize={4} lineHeight={4} fontWeight={1} color='primary' style={{ position: 'fixed' }}>
-          40%
-        </Typography>
-      ) : (
-        ''
-      )}
-    </StyledLoaderSpinner>
+    <LoaderContainer percentage={percentage} variant='icon'>
+      <svg>
+        <circle cx='60' cy='60' r='50' />
+        <circle cx='60' cy='60' r='50' />
+      </svg>
+      <div id='counter' className='counter'></div>
+      <div className='stop'></div>
+      <div className='progress'>Progress Bar</div>
+    </LoaderContainer>
   )
 }
 
@@ -31,50 +68,92 @@ Loader.propTypes = {
 }
 
 Loader.defaultProps = {
-  showPercentage: false,
-  size: 'big'
+  showPercentage: false
 }
 
-const StyledLoaderSpinner = styled.div`
-  /* position: fixed; */
-  /* z-index: 10; */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* top: 0; */
-  /* left: 0; */
+const LoaderContainer = styled.div`
+  /* margin-top: 100px; */
+  height: 350px;
+  width: 250px;
+  background: gray.800;
+  position: relative;
 
-  /* &:before {
-    background: #666;
-    position: absolute;
-    z-index: 9;
-    opacity: 0.8;
-    content: '';
-    height: 100%;
-    width: 100%;
-  } */
-`
+  svg {
+    width: 120px;
+    height: 120px;
+    position: relative;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%) rotate(-90deg);
+    fill: transparent;
+  }
 
-const Spinner = styled.div`
-  z-index: 11;
-  border-style: solid;
-  border-color: blue.400;
-  /* height: 76.8px;
-  width: 76.8px; */
-  border-right-color: rgb(192, 212, 235);
-  border-radius: 50%;
-  animation: spin 5s linear infinite;
-  /* position: absolute; */
-  opacity: 1;
+  svg circle:nth-child(1) {
+    stroke: #1565c0;
+    opacity: 0.25;
+    stroke-width: 10px;
+  }
 
-  ${layout}
-  ${borders}
+  svg circle:nth-child(2) {
+    stroke: #1565c0;
+    stroke-width: 10px;
+    stroke-linecap: round;
+    stroke-dasharray: 500;
+    stroke-dashoffset: 500;
+    animation: animate ${props => props.percentage}s linear infinite;
+  }
 
-  @keyframes spin {
-    to {
-      transform: rotate(360deg);
+  @keyframes animate {
+    100% {
+      stroke-dashoffset: 0;
     }
   }
+
+  .stop {
+    height: 110px;
+    width: 110px;
+    background: transparent;
+    border-radius: 50%;
+    border: 10px solid #0f0;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    display: none;
+  }
+
+  .progress {
+    position: absolute;
+    bottom: 60px;
+    width: 100%;
+    text-align: center;
+    font-size: 25px;
+    font-weight: 800;
+    color: #0f0;
+    font-weight: 800;
+    font-family: sans-serif;
+  }
+
+  .counter {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 20px;
+    font-weight: 800;
+  }
+
+  ${variant({
+    default: 'icon',
+    variants: {
+      icon: css`
+        svg circle {
+          stroke: #ff00ff !important;
+        }
+      `,
+      major: css``
+    }
+  })}
 `
 
 export default Loader
