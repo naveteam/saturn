d56add24d27c66adf8f2c4dbc6bb4f360a742f38
Thiago Nunes Batista <thiago.batista@nave.rs>
2020-08-24 09:05:37 -0300

    Update index.js
    remove trash from rebase

--- src/components/index.js
+++ src/components/index.js
@@ -4,8 +4,5 @@ export * from './Grid'
 export * from './Tag'
 export * from './Button'
 export * from './Iconography'
-<<<<<<< HEAD
 export * from './Breadcrumb'
-=======
 export * from './Checkbox'
->>>>>>> feat(checkbox): add component with internal onChange
