3b96250b0a019d7e389910c704a9b856cd74b68f
Glauber Brack <glauber@brack.com.br>
2020-09-03 15:16:23 -0300

    refactor(table): abstraction TableRow from Table component

--- src/components/Table/TableRow.jsx
+++ src/components/Table/TableRow.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import styled from '@xstyled/styled-components'
 import { th } from '@xstyled/system'
 
@@ -8,6 +9,19 @@ const TableRow = styled.tr`
   border-bottom: 1px solid ${th.color('gray.300')};
   align-items: center;
   justify-content: center;
+=======
+import React from 'react'
+import styled from '@xstyled/styled-components'
+import PropTypes from 'prop-types'
+
+import { Typography } from '../'
+
+const TableRow = ({ children }) => <Container>{children}</Container>
+
+const Container = styled.tr`
+  padding: 16px;
+  border-bottom: 1px solid #e0e0e0;
+>>>>>>> refactor(table): abstraction TableRow from Table component
 `
 
 export default TableRow
