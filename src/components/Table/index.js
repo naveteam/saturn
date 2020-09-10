cdb98f6072946f7ef8067aef2dec7f47a1f14aff
glauberbrack <glauber@brack.com.br>
2020-09-10 13:47:55 -0300

    refactor(table): removing avatar as a table component

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,9 +1,12 @@
-import Avatar from './Avatar'
 import Table from './Table'
 import TableContent from './TableContent'
 import TableData from './TableData'
 import TableRow from './TableRow'
 
+<<<<<<< HEAD
 export { Table, TableContent, TableData, TableRow }
+=======
+export { Table, TableBody, TableData, TableHeader, TableRow }
+>>>>>>> refactor(table): removing avatar as a table component
 
 export default Table
