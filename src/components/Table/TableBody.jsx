03e275e6512afb66c75860af569e16cb50b59f7d
glauberbrack <glauber@brack.com.br>
2020-09-10 13:45:17 -0300

    refactor(tablebody): add color from props

--- src/components/Table/TableBody.jsx
+++ src/components/Table/TableBody.jsx
@@ -1,7 +1,8 @@
 import styled from '@xstyled/styled-components'
+import { th } from '@xstyled/system'
 
 const TableBody = styled.tbody`
-  border-top: 2px solid #e0e0e0;
+  border-top: 2px solid ${th.color('gray.100')};
 `
 
 export default TableBody
