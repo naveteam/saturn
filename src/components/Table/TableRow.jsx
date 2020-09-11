bdb047047937ffcc8b299f8a5101d6fdb886518e
Gustavo Pinho <gustavo@nave.rs>
2020-09-11 13:51:43 -0300

    fix(table): fix table component structure

--- src/components/Table/TableRow.jsx
+++ src/components/Table/TableRow.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled from '@xstyled/styled-components'
 import { th } from '@xstyled/system'
 
@@ -9,16 +8,8 @@ const TableRow = styled.tr`
   border-bottom: 1px solid ${th.color('gray.300')};
   align-items: center;
   justify-content: center;
-=======
-import React from 'react'
-import styled from '@xstyled/styled-components'
-
-const TableRow = ({ children }) => <Container>{children}</Container>
-
-const Container = styled.tr`
-  padding: 16px;
   border-bottom: 1px solid #e0e0e0;
->>>>>>> refactor(table): abstraction TableRow from Table component
+  border-bottom: 1px solid ${th.color('gray.300')};
 `
 
 export default TableRow
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,10 +3,6 @@ import TableContent from './TableContent'
 import TableData from './TableData'
 import TableRow from './TableRow'
 
-<<<<<<< HEAD
 export { Table, TableContent, TableData, TableRow }
-=======
-export { Table, TableBody, TableData, TableHeader, TableRow }
->>>>>>> refactor(table): removing avatar as a table component
 
 export default Table
