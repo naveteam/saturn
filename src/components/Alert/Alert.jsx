fe5cd263858f21c320629bf658ae395652b1fa93
glauberbrack <glauber@brack.com.br>
2020-09-15 09:37:40 -0300

    feat(alert): create alert component

---
+++ src/components/Alert/Alert.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled, { css } from '@xstyled/styled-components'
+import { variant } from '@xstyled/system'
+
+import { Typography } from './'
+
+const Alert = () => <div>um alertinha</div>
+
+export default Alert
