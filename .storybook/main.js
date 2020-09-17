20e6dbb5920d423f28a2aca83983ae15d97a8d04
Italo Nolasco <italo.nolasco@nave.rs>
2020-09-17 11:56:31 -0300

    feat(storybook/viewport): allow to change the screen resolution
    Permite trocar entre diferentes resoluçoes para realizar o teste

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,8 @@ module.exports = {
   stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
     '@storybook/addon-docs',
+    '@storybook/addon-viewport',
     'storybook-addon-designs/register',
     'storybook-svgr-react-component'
-  ],
+  ]
 }
