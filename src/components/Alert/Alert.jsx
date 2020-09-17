b19b51b441d0f00f55616a46e80098779a6c8be4
glauberbrack <glauber@brack.com.br>
2020-09-17 14:44:34 -0300

    fix(alert): using react-toastify

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -4,105 +4,114 @@ import { th, variant, compose, color, layout, space, border } from '@xstyled/sys
 import { Typography } from '../'
 import { Icon } from '../Iconography'
 
-const Alert = ({ type, position, title, text, closeIcon }) => (
-  <Base type={type} position={position}>
-    <Header>
-      <LeftHeader>
+import { ToastContainer, toast as alert } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const Alert = ({ type, position, title, text, closeIcon }) => {
+  const body = (
+    <div>
+      <span>
         <Icon icon={type} color='white' height='20' />
         <Title>{title}</Title>
-      </LeftHeader>
+      </span>
+      <Description>{text}</Description>
+    </div>
+  )
 
-      {closeIcon && (
-        <CloseButton onClick={() => {}}>
-          <Icon icon='close' color='white' height='24' />
-        </CloseButton>
-      )}
-    </Header>
+  const options = {
+    position: position === 'top' ? 'top-center' : 'top-right',
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: closeIcon ? true : false,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined
+  }
 
-    {text && <Description>{text}</Description>}
-  </Base>
-)
+  const notify = () => {
+    switch (type) {
+      case 'success':
+        alert.success(body, options)
+        break
+      case 'warning':
+        alert.warning(body, options)
+        break
+      case 'error':
+        alert.error(body, options)
+        break
+      default:
+        alert.info(body, options)
+    }
+  }
 
-const typeVariant = variant({
-  prop: 'type',
-  default: 'info',
+  return (
+    <div>
+      <button onClick={notify}>Info Alert</button>
+      <Container closeIcon={closeIcon} />
+    </div>
+  )
+}
+
+const closeVariant = variant({
+  prop: 'closeIcon',
   variants: {
-    info: css`
-      background: ${th.color('blue.400')};
+    true: css`
+      display: flex;
     `,
-    success: css`
-      background: ${th.color('green.400')};
-    `,
-    error: css`
-      background: ${th.color('red.400')};
-    `,
-    warning: css`
-      background: ${th.color('amber.400')};
+    false: css`
+      display: none;
     `
   }
 })
 
-const positionVariant = variant({
-  prop: 'position',
-  variants: {
-    right: css`
-      animation: appearFromRight 0.7s;
-    `,
-    top: css`
-      animation: appearFromTop 0.7s;
-    `
+const Container = styled(ToastContainer)`
+  .Toastify__toast-container {
+    width: 384px;
   }
-})
 
-const Base = styled.div`
-  ${typeVariant}
-  display: flex;
-  width: 384px;
-  flex-direction: column;
-  border-radius: 2;
-  padding: 4;
-  ${positionVariant}
-
-  @keyframes appearFromRight {
-    from {
-      transform: translateX(100%);
-    }
-    to {
-      transform: translateX(0);
+  .Toastify__toast {
+    border-radius: 2;
+    padding: 4;
+    width: 384px;
+    div {
+      margin-top: 0;
+      span {
+        display: flex;
+        align-items: center;
+        margin-top: 0;
+        margin-bottom: 0;
+      }
     }
   }
 
-  @keyframes appearFromTop {
-    from {
-      transform: translateY(-100%);
-    }
-    to {
-      transform: translateY(0);
-    }
+  .Toastify__toast--info {
+    right: 4em;
+    background: ${th.color('blue.400')};
+  }
+  .Toastify__toast--success {
+    right: 4em;
+    background: ${th.color('green.400')};
+  }
+  .Toastify__toast--error {
+    right: 4em;
+    background: ${th.color('red.400')};
+  }
+  .Toastify__toast--warning {
+    right: 4em;
+    background: ${th.color('amber.400')};
+  }
+
+  .Toastify__close-button {
+    ${closeVariant}
   }
 `
-const Header = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`
-const LeftHeader = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`
+
 const Title = styled(Typography)`
   font-weight: 1;
   font-size: 3;
   line-height: 3;
   color: white;
   margin-left: 3;
-`
-
-const CloseButton = styled.button`
-  background: none;
-  border: 0;
 `
 
 const Description = styled(Typography)`
