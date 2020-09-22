8fa4c8e6ef857f3d8b5c34c4b70832d8a63f49f6
vitor.tavares.nave <vitor.tavares@nave.rs>
2020-09-22 18:42:01 -0300

    feat(attachment): added props uses, component styling and documentation

--- src/components/Attachment/Attachment.jsx
+++ src/components/Attachment/Attachment.jsx
@@ -1,28 +1,95 @@
 import PropTypes from 'prop-types'
-import React, { forwardRef } from 'react'
-import styled, { css, down, typography } from '@xstyled/styled-components'
+import React, { useState } from 'react'
+import styled from '@xstyled/styled-components'
+import { space, layout, variant, th } from '@xstyled/system'
 
 import { Typography } from '../'
 import { Icon } from '../Iconography'
 import { Flex } from '../Grid'
 
-const Attachment = ({ name, link, onDownload, onView, file, backgroundColor, error, ...props }) => {
+const Attachment = ({ name, link, onDownload, onView, onDelete, file, backgroundColor, error, variant, ...props }) => {
+  const [showIconsOnHover, setShowIconsOnHover] = useState(false)
+
+  const handleErrorColor = () => (error && variant === 'upload' ? 'error' : 'primary')
+
   return (
-    <Flex px='4px' backgroundColor={backgroundColor}>
-      <Flex justifyContent='flex-start'>
-        <StyledIcon width={16} height={24} pr='8px' icon='attachment' />
-        <Typography>{name}</Typography>
+    <Wrapper
+      backgroundColor={backgroundColor}
+      onMouseEnter={() => setShowIconsOnHover(true)}
+      onMouseLeave={() => setShowIconsOnHover(false)}
+    >
+      <Flex mr={24}>
+        <AttachmentIcon color={handleErrorColor()} width={16} height={24} icon='attachment' />
+        <Link color={handleErrorColor()} forwardedAs='a' pl='8px' {...(link && { href: link, target: '_blank' })}>
+          {file ? (
+            <Flex>
+              {file?.name} {file?.size && <Typography pl='8px'>({file?.size})</Typography>}
+            </Flex>
+          ) : (
+            name
+          )}
+        </Link>
       </Flex>
-      <Flex justifyContent='flex-end' ml={24}>
-        <Icon width={16} height={24} pr='8px' icon='visibility-outline' />
-        <Icon width={16} height={24} icon='delete-outline' />
-      </Flex>
-    </Flex>
+
+      {variant === 'download' ? (
+        <Flex>
+          <StyledIcon icon='download' onClick={onDownload} />
+          <StyledIcon icon='visibility-outline' onClick={onView} />
+          <StyledIcon icon='delete-outline' onClick={onDelete} />
+        </Flex>
+      ) : (
+        showIconsOnHover && (
+          <Flex>
+            {error && variant === 'upload' ? (
+              <StyledIcon icon='delete-outline' onClick={onDelete} color={handleErrorColor()} />
+            ) : (
+              <Flex>
+                <StyledIcon icon='visibility-outline' onClick={onView} />
+                <StyledIcon icon='delete-outline' onClick={onDelete} color={handleErrorColor()} />
+              </Flex>
+            )}
+          </Flex>
+        )
+      )}
+    </Wrapper>
   )
 }
 
+const Wrapper = styled(Flex)`
+  justify-content: space-between;
+  padding-right: 4px;
+  padding-left: 4px;
+  ${layout}
+  ${space}
+`
+
 const StyledIcon = styled(Icon)`
+  fill: primary;
+  width: 16px;
+  height: 24px;
+  padding-right: 8px;
+
+  &:last-child {
+    padding-right: 0px;
+  }
+
+  :hover {
+    cursor: pointer;
+  }
+`
+
+const Link = styled(Typography)`
+  cursor: pointer;
+  text-decoration: none;
+
+  :hover {
+    text-decoration: underline;
+  }
+`
+
+const AttachmentIcon = styled(Icon)`
   transform: rotate(-45deg);
+  fill: primary;
 `
 
 Attachment.propTypes = {
@@ -30,9 +97,20 @@ Attachment.propTypes = {
   link: PropTypes.string,
   onDownload: PropTypes.func,
   onView: PropTypes.func,
-  file: PropTypes.string,
+  onDelete: PropTypes.func,
+  file: PropTypes.object,
   backgroundColor: PropTypes.string,
-  error: PropTypes.string
+  error: PropTypes.bool,
+  variant: PropTypes.oneOf(['upload', 'download'])
+}
+
+Attachment.defaultProps = {
+  name: 'anexo1.png',
+  error: false,
+  onDownload: () => alert('download'),
+  onView: () => alert('view'),
+  onDelete: () => alert('delete'),
+  variant: 'upload'
 }
 
 export default Attachment
