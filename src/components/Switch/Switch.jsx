d9900ebe00f0050df05baa7e4b9fce2f36f1b1b0
Rafael Schumacher <54547117+rbschumacher@users.noreply.github.com>
2020-09-30 10:32:32 -0300

    Feat: Switch component (#102)
    * feat(switch): add first structure of switch component
    
    * feat(switch): add onChange and documentation
    
    * refactor(switch): export pattern and props on styled

---
+++ src/components/Switch/Switch.jsx
@@ -0,0 +1,109 @@
+import React, { useState, forwardRef } from 'react'
+import styled, { css, variant } from '@xstyled/styled-components'
+import PropTypes from 'prop-types'
+
+import { Flex, Box } from '../Grid'
+import Typography from '../Typography'
+
+const Switch = forwardRef(({ name, label, value, disabled, onChange, defaultChecked, checked, id, ...props }, ref) => {
+  const [isEnabled, setIsEnabled] = useState(defaultChecked)
+  return (
+    <Flex alignItems='center' {...props}>
+      <SwitchContainer enabled={isEnabled} disabled={disabled}>
+        <HiddenInput
+          ref={ref}
+          type='checkbox'
+          name={name}
+          id={id}
+          value={value}
+          onChange={e => {
+            setIsEnabled(e.target.checked)
+            if (onChange) onChange(e)
+          }}
+          disabled={disabled}
+          defaultChecked={defaultChecked}
+          checked={checked}
+        />
+        <Controller enabled={isEnabled} />
+      </SwitchContainer>
+      {label && <Typography color={disabled ? 'disabled' : 'gray.900'}>{label}</Typography>}
+    </Flex>
+  )
+})
+
+const colorVariants = variant({
+  default: false,
+  prop: 'enabled',
+  variants: {
+    true: css`
+      background-color: primary;
+    `,
+
+    false: css`
+      background-color: gray.700;
+    `
+  }
+})
+
+const positionVariant = variant({
+  default: false,
+  prop: 'enabled',
+  variants: {
+    true: css`
+      left: 18px;
+    `,
+    false: css`
+      left: 2px;
+    `
+  }
+})
+
+const SwitchContainer = styled.label(
+  ({ disabled }) => css`
+    width: 32px;
+    height: 16px;
+    border-radius: 8px;
+    position: relative;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+    margin-right: 8px;
+
+    ${disabled
+      ? css`
+          background-color: gray.400;
+          cursor: not-allowed;
+        `
+      : colorVariants}
+  `
+)
+
+const HiddenInput = styled.input`
+  display: none;
+`
+
+const Controller = styled(Box)`
+  width: 12px;
+  height: 12px;
+  background-color: white;
+  border-radius: 50%;
+  position: absolute;
+  top: 2px;
+  left: 2px;
+
+  transition: all 0.3s ease-in-out;
+
+  ${positionVariant}
+`
+
+Switch.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool]),
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  defaultChecked: PropTypes.bool,
+  checked: PropTypes.any,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+}
+
+export default Switch
