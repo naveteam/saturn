df317f4dc8f5a9e6db94a1fb04e3df0e9c69fa07
vitor.tavares.nave <vitor.tavares@nave.rs>
2020-09-30 13:56:51 -0300

    feat(upload): added image upload variant to upload component

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState, useEffect } from 'react'
 import styled from '@xstyled/styled-components'
 import PropTypes from 'prop-types'
 
-import { Typography, Flex, Button } from '../'
+import { Flex, Button, Icon } from '../'
 
 const handleAcceptedFileTypes = fileTypes => {
   return typeof fileTypes === 'object' ? fileTypes.join(',') : fileTypes
@@ -100,10 +100,12 @@ const UploadImage = ({ caption, acceptedFileTypes, disabled, ...props }) => {
   const [uploadedImage, setUploadedImage] = useState(null)
   const [imagePreview, setImagePreview] = useState(null)
 
-  const handleClick = () => hiddenFileInput.current.click()
+  const handleClickImageUpload = () => hiddenFileInput.current.click()
+
   const handleChange = event => {
-    setUploadedImage(event.target.files)
+    setUploadedImage(event.target.files[0])
     setImagePreview(URL.createObjectURL(event.target.files[0]))
+    event.target.value = null
   }
 
   return (
@@ -115,11 +117,25 @@ const UploadImage = ({ caption, acceptedFileTypes, disabled, ...props }) => {
         accept={handleAcceptedFileTypes(acceptedFileTypes)}
       />
       {imagePreview ? (
-        <StyledImage src={imagePreview} p={3} />
+        <ImageContainer p={3}>
+          <ImageOverlay>
+            <Icon icon='visibility-outline' color='white' mr={2} onClick={() => window.open(imagePreview, '_blank')} />
+            <Icon
+              icon='delete-outline'
+              color='white'
+              ml={2}
+              onClick={() => {
+                setImagePreview(null)
+                setUploadedImage(null)
+              }}
+            />
+          </ImageOverlay>
+          <StyledImage src={imagePreview} />
+        </ImageContainer>
       ) : (
         <ImageUpload
           py={5}
-          onClick={handleClick}
+          onClick={handleClickImageUpload}
           disabled={disabled}
           icon='upload'
           direction='column'
@@ -175,12 +191,44 @@ const Upload = ({ caption, description, variant, acceptedFileTypes, multipleFile
     )
 }
 
-const StyledImage = styled.img`
+const ImageOverlay = styled(Flex)`
+  margin: auto;
+  justify-content: center;
+  align-items: center;
+  background: #212121;
+  border-radius: 4px;
+  width: 262px;
+  height: 262px;
+  position: absolute;
+  opacity: 0;
+  transition: 0.2s ease;
+`
+
+const ImageContainer = styled(Flex)`
   width: 282px;
   height: 282px;
   border: 2px #1565c0 dashed;
   border-radius: 4px;
-  padding: 8px;
+  margin: auto;
+  :hover {
+    border: 2px #114d91 dashed;
+    ${ImageOverlay} {
+      opacity: 0.8;
+      ${Icon} {
+        opacity: 1;
+        cursor: pointer;
+      }
+    }
+  }
+  :active {
+    border: 2px #0c3563 dashed;
+  }
+`
+
+const StyledImage = styled.img`
+  object-fit: cover;
+  width: 100%;
+  border-radius: 4px;
 `
 
 const StyledButton = styled(Button)`
