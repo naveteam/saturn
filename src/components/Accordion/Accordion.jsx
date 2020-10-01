91f9b6b22a709f2ee43ebea1177cf8f772420f06
Thiago Nunes Batista <thiago.batista@nave.rs>
2020-10-01 09:16:53 -0300

    feat(accordion): remove unused imports

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React, { cloneElement, Children, useState } from 'react'
-import styled, { backgrounds, css, space, variant } from '@xstyled/styled-components'
+import styled, { backgrounds, space } from '@xstyled/styled-components'
 import { Icon, Typography } from '../'
 
 const Accordion = ({ children, disabled = false, expanded: propsExpanded = false }) => {
