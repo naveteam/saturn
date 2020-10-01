57bca9ed5bdd7b2e814fe93aac108b76bc4746b4
Ítalo Nolasco <italo.nolasco@nave.rs>
2020-10-01 11:37:54 -0300

    feat(addon-controls): add addon that allows change dynamically props (#124)

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
   addons: [
     '@storybook/addon-docs',
     '@storybook/addon-viewport',
+    '@storybook/addon-controls',
     'storybook-addon-designs/register',
     'storybook-svgr-react-component'
   ]
