91730ef78859b91064aacc203a66d16e8d06a440
Brian Icaro D. Marques <brian.icaro@nave.rs>
2020-10-08 16:37:34 -0300

    feat(component): finished component arrow direction and position
    feature #38

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -6,23 +6,83 @@ import PropTypes from 'prop-types'
 import { Caption } from '../'
 import Icon from '../Iconography'
 
-const Tooltip = forwardRef(({ children, tooltip, variant, ...props }, ref) => (
-  <Wrapper direction={props.direction} ref={ref} {...props}>
-    {children}
+const Tooltip = forwardRef(({ children, variant, direction, ...props }, ref) => {
+  console.log('props', props, 'variant:', variant, 'direction:', direction)
+  return (
+    <Wrapper ref={ref} {...props}>
+      {children}
 
-    {props.small ? (
-      <SmallTip variant={variant}>
-        {props.icon && <Icon color='white' icon={props.icon} />}
-        {<Caption variant='sm'>{props.content}</Caption>}
-      </SmallTip>
-    ) : (
-      <Tip variant={variant}>
-        {props.icon && <Icon color='white' icon={props.icon} />}
-        <Caption>{props.content}</Caption>
-      </Tip>
-    )}
-  </Wrapper>
-))
+      {props.small ? (
+        <SmallTip variant={variant} direction={direction}>
+          {props.icon && <Icon color='white' icon={props.icon} />}
+          {<Caption variant='sm'>{props.content}</Caption>}
+        </SmallTip>
+      ) : (
+        <Tip variant={variant} direction={direction}>
+          {props.icon && <Icon color='white' icon={props.icon} />}
+          <Caption>{props.content}</Caption>
+        </Tip>
+      )}
+    </Wrapper>
+  )
+})
+
+const directionVariants = variant({
+  default: 'up',
+  prop: 'direction',
+  variants: {
+    'upper-left': css`
+      :before {
+        left: calc(25% - 8px);
+      }
+    `,
+    up: css`
+      :before {
+        left: calc(50% - 8px);
+      }
+    `,
+    'upper-right': css`
+      :before {
+        left: calc(75% - 8px);
+      }
+    `,
+    left: css`
+      :before {
+        top: calc(50% - 6px);
+        left: -10px;
+        transform: rotate(270deg);
+      }
+    `,
+    right: css`
+      :before {
+        top: calc(50% - 6px);
+        right: -10px;
+        transform: rotate(90deg);
+      }
+    `,
+    'lower-left': css`
+      :before {
+        top: calc(100%);
+        left: calc(25% - 8px);
+        transform: rotate(180deg);
+      }
+    `,
+    down: css`
+      :before {
+        top: calc(100%);
+        left: calc(50% - 8px);
+        transform: rotate(180deg);
+      }
+    `,
+    'lower-right': css`
+      :before {
+        top: calc(100%);
+        left: calc(75% - 8px);
+        transform: rotate(180deg);
+      }
+    `
+  }
+})
 
 const Tip = styled.div`
   visibility: hidden;
@@ -43,7 +103,6 @@ const Tip = styled.div`
 
   :before {
     content: '';
-    left: calc(50% - 8px);
     height: 0px;
     width: 0px;
     position: absolute;
@@ -52,6 +111,7 @@ const Tip = styled.div`
     border-bottom: 10px solid ${({ variant }) => th.color(variant)};
     top: -10px;
   }
+  ${directionVariants}
 `
 
 const SmallTip = styled(Tip)`
@@ -73,7 +133,7 @@ Tooltip.propTypes = {
   color: PropTypes.string,
   content: PropTypes.string,
   direction: PropTypes.oneOf(['up', 'down', 'left', 'right', 'upper-left', 'upper-right', 'lower-left', 'lower-right']),
-  variant: PropTypes.oneOf(['neutral', 'semantic', 'smaller'])
+  variant: PropTypes.oneOf(['neutral', 'smaller', 'informative', 'success', 'warning', 'error', 'support'])
 }
 
 export default Tooltip
