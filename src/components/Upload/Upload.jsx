aceb6609f9ad971805af30011126b258bafd3b87
Matheus Cruz <matheus.cruz@nave.rs>
2020-10-21 14:25:41 -0300

    style(ui): it changes some styles requested by design review

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -276,7 +276,7 @@ const StyledImage = styled.img`
 
 const StyledButton = styled(Button)`
   border-color: ${props => props.theme.colors.primary};
-  border-width: 2px;
+  border-width: 1px;
   border-style: ${props => (props.uploadedFiles.length === 0 ? 'dashed' : 'solid')};
 `
 const ImageUpload = styled(Button)`
