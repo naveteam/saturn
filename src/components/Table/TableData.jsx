568116492793b395cee4b4bd48ebace7eacc0a11
João Pedro Bretanha <joaopedro@nave.rs>
2020-11-15 22:12:03 -0300

    fix: table tds with font properties

--- src/components/Table/TableData.jsx
+++ src/components/Table/TableData.jsx
@@ -1,9 +1,12 @@
 import styled from '@xstyled/styled-components'
 
 const TableData = styled.td`
-  min-height: 48px;
-
-  padding: 0 16px;
+  font-size: 0.875rem;
+  line-height: 1.43;
+  padding: 12px;
+  letter-spacing: 0.01071em;
+  vertical-align: inherit;
+  font-family: 'Open Sans',sans-serif;
 `
 
 export default TableData
