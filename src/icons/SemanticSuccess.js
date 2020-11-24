66d8364fba98a1bcb330e9dc41d11c277a490b51
Glauber Brack <57924169+glauberbrack@users.noreply.github.com>
2020-11-24 10:47:40 -0300

    feat/alert (#118)
    * feat(alert): create alert component
    
    * feat(alert): create story
    
    * feat(alert): initialize component
    
    * feat(alert): props on story
    
    * feat(alert): styles on Alert component
    
    * feat(alert): button for close and animated css
    
    * chore: add react-toastify
    
    * fix(alert): using react-toastify
    
    * feat(alert preview): create component to render alert
    
    * fix(alert): fix on close icon
    
    * feat(alert): definition of proptypes
    
    * fix(alert): remove alert old component
    
    * refactor(alert): removing unuseless importings
    
    * feat(alert): create alert component
    
    * feat(alert): create story
    
    * feat(alert): initialize component
    
    * feat(alert): props on story
    
    * feat(alert): styles on Alert component
    
    * feat(alert): button for close and animated css
    
    * chore: add react-toastify
    
    * fix(alert): using react-toastify
    
    * feat(alert preview): create component to render alert
    
    * fix(alert): fix on close icon
    
    * feat(alert): definition of proptypes
    
    * fix(alert): remove alert old component
    
    * refactor(alert): removing unuseless importings
    
    Co-authored-by: Matheus Cruz <devmatheusc@gmail.com>

---
+++ src/icons/SemanticSuccess.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+function SvgSemanticSuccess(props) {
+  return (
+    <svg viewBox='0 0 24 24' width='1em' height='1em' {...props}>
+      <path
+        fill-rule='evenodd'
+        clip-rule='evenodd'
+        d='M2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12ZM7.8327 12.154L9.924 14.2453L15.8785 8.29686C16.1135 8.06181 16.4932 8.06181 16.7282 8.29686C16.9633 8.5319 16.9633 8.91159 16.7282 9.14664L10.3519 15.523C10.1169 15.758 9.73717 15.758 9.50212 15.523L6.98292 13.0038C6.74788 12.7687 6.74788 12.389 6.98292 12.154C7.21797 11.919 7.59766 11.919 7.8327 12.154Z'
+        fill='#43A047'
+      />
+    </svg>
+  )
+}
+
+export default SvgSemanticSuccess
