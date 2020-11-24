f6ce8666bc3da2d02e687b180f8ae23536ee0b41
Ítalo Nolasco <italo.nolasco@nave.rs>
2020-11-24 10:13:52 -0300

    feat(alert): new pr to alert

---
+++ src/components/Alert/AlertPreview.jsx
@@ -0,0 +1,131 @@
+import React from 'react'
+import styled, { css } from '@xstyled/styled-components'
+import { th, variant } from '@xstyled/system'
+import { Typography, Button } from '../'
+import { Icon } from '../Iconography'
+import PropTypes from 'prop-types'
+
+import { ToastContainer, toast as alert } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const AlertPreview = ({ type, position, title, text, closeIcon }) => {
+  const body = (
+    <div>
+      <span>
+        <Icon icon={`semantic_${type}`} color='white' height='20' />
+        <Title>{title}</Title>
+      </span>
+      <Description>{text}</Description>
+    </div>
+  )
+
+  const options = {
+    position: position === 'top' ? 'top-center' : 'top-right',
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: closeIcon ? true : false,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined
+  }
+
+  const notify = () => {
+    switch (type) {
+      case 'success':
+        alert.success(body, options)
+        break
+      case 'warning':
+        alert.warning(body, options)
+        break
+      case 'error':
+        alert.error(body, options)
+        break
+      default:
+        alert.info(body, options)
+    }
+  }
+
+  return (
+    <>
+      <Button width='350px' onClick={notify} style={{ margin: '5px' }}>
+        Clique para ver o alerta: {type}
+      </Button>
+      <Container closeIcon={closeIcon} />
+    </>
+  )
+}
+const closeVariant = variant({
+  prop: 'closeIcon',
+  variants: {
+    true: css`
+      display: flex;
+    `,
+    false: css`
+      display: none;
+    `
+  }
+})
+const Container = styled(ToastContainer)`
+  .Toastify__toast-container {
+    width: 384px;
+  }
+  .Toastify__toast {
+    border-radius: 2;
+    padding: 4;
+    width: 384px;
+    div {
+      margin-top: 0;
+      span {
+        display: flex;
+        align-items: center;
+        margin-top: 0;
+        margin-bottom: 0;
+      }
+    }
+  }
+  .Toastify__toast--info {
+    right: 4em;
+    background: ${th.color('blue.400')};
+  }
+  .Toastify__toast--success {
+    right: 4em;
+    background: ${th.color('green.400')};
+  }
+  .Toastify__toast--error {
+    right: 4em;
+    background: ${th.color('red.400')};
+  }
+  .Toastify__toast--warning {
+    right: 4em;
+    background: ${th.color('amber.400')};
+  }
+  .Toastify__close-button {
+    ${closeVariant}
+  }
+`
+const Title = styled(Typography)`
+  font-weight: 1;
+  font-size: 3;
+  line-height: 3;
+  color: white;
+  margin-left: 3;
+`
+const Description = styled(Typography)`
+  margin-top: 3;
+  font-size: 3;
+  line-height: 3;
+  color: white;
+`
+AlertPreview.defaultProps = {
+  type: 'info',
+  closeIcon: true,
+  position: 'top'
+}
+AlertPreview.propTypes = {
+  type: PropTypes.oneOf(['info', 'success', 'warning', 'error']),
+  position: PropTypes.oneOf(['top', 'right']),
+  closeIcon: PropTypes.oneOf([true, false]),
+  title: PropTypes.string,
+  text: PropTypes.string
+}
+export default AlertPreview
---
+++ src/components/Alert/index.js
@@ -0,0 +1,6 @@
+import Alert from './Alert'
+import AlertPreview from './AlertPreview'
+
+export { Alert, AlertPreview }
+
+export default Alert
