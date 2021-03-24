995d4ee64d901464ca156d3d6ea7ce22ff8dca2a
Rafael Schumacher <54547117+rbschumacher@users.noreply.github.com>
2021-03-24 08:58:30 -0300

    feat(tag): add props pattern (#186)
    * feat(tag): add props pattern
    
    * feat(tag): change component structure
    
    * fix(tag): add return to handle click function

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,20 +1,60 @@
-import React from 'react'
+import React, { useMemo, useCallback } from 'react'
 import styled, { css } from '@xstyled/styled-components'
 import { th, variant, compose, color, layout, space, border } from '@xstyled/system'
 import PropTypes from 'prop-types'
-import { Typography } from '../'
-import { Icon } from '../Iconography'
 
-const Tag = ({ children, close, ...props }) => (
-  <Base {...props}>
-    <Content>
-      <Text padding={close ? '3px 0 3px 4px' : '3px 4px 3px 4px'} lineHeight={1}>
-        {children}
-      </Text>
-      {close && <Icon icon='clear' color='white' height='16' />}
-    </Content>
-  </Base>
-)
+import { Typography, Icon } from '../'
+
+const Tag = ({ children, close, closable, variant, selected, disabled, value, onChange, onClose, type, ...props }) => {
+  const tagVariant = useMemo(() => {
+    if (disabled || selected === 'disabled' || variant === 'disabled') {
+      return 'disabled'
+    }
+
+    if (type === 'selectable') {
+      if (value) {
+        return 'selected'
+      } else {
+        return 'unselected'
+      }
+    }
+
+    if (variant) {
+      return variant
+    }
+
+    if (selected === false || selected === 'unselected') {
+      return 'unselected'
+    }
+
+    return 'selected'
+  }, [variant, selected, type, disabled, value])
+
+  const handleClick = useCallback(() => {
+    if (disabled) {
+      return
+    }
+
+    if ((closable || close) && onClose) {
+      return onClose()
+    }
+
+    if (type === 'selectable' && onChange) {
+      return onChange()
+    }
+  }, [type, closable, close, onClose, onChange])
+
+  return (
+    <Base selected={tagVariant} onClick={handleClick} {...props}>
+      <Content>
+        <Text padding={close || closable ? '3px 0 3px 4px' : '3px 4px 3px 4px'} lineHeight={1}>
+          {children}
+        </Text>
+        {(close || closable) && <Icon icon='clear' color='white' height='16' />}
+      </Content>
+    </Base>
+  )
+}
 
 const baseProps = compose(color, layout, space, border)
 
@@ -23,18 +63,16 @@ const selectedVariant = variant({
   prop: 'selected',
   key: 'tag',
   variants: {
-    true: css`
-      background-color: ${({ color }) => th.color(color)};
-      border-color: ${({ color }) => th.color(color)};
-      cursor: pointer;
+    selected: css`
+      background-color: ${({ color, colorScheme }) => th.color(colorScheme || color || 'primary')};
+      border-color: ${({ color, colorScheme }) => th.color(colorScheme || color || 'primary')};
       p {
         color: white;
       }
     `,
-    false: css`
+    unselected: css`
       background-color: transparent;
       border-color: gray.600;
-      cursor: pointer;
       p {
         color: gray.600;
       }
@@ -42,7 +80,7 @@ const selectedVariant = variant({
     disabled: css`
       background-color: disabled;
       border-color: disabled;
-      cursor: default;
+      cursor: not-allowed;
       p {
         color: white;
       }
@@ -50,13 +88,15 @@ const selectedVariant = variant({
   }
 })
 
-const Base = styled.div`
+const Base = styled.button`
   display: inline-block;
   border-radius: 1;
   border-width: 1px;
   border-style: solid;
-  ${baseProps}
-  ${selectedVariant}
+  outline: none;
+  cursor: pointer;
+  ${baseProps};
+  ${selectedVariant};
 `
 
 const Content = styled.div`
@@ -71,15 +111,18 @@ const Text = styled(Typography)`
 `
 
 Tag.defaultProps = {
-  selected: true,
-  close: false,
-  color: 'primary'
+  close: false
 }
 
 Tag.propTypes = {
-  selected: PropTypes.oneOf([true, false, 'disabled']),
   close: PropTypes.bool,
-  color: PropTypes.string
+  color: PropTypes.string,
+  colorScheme: PropTypes.string,
+  type: PropTypes.oneOf(['info', 'selectable']),
+  value: PropTypes.bool,
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  onClose: PropTypes.func
 }
 
 export default Tag
