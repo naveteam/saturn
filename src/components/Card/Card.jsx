487d3b0319dfa8c9f297df006d1eea0f18e30fbf
coelhomariana <coelhomariana@outlook.com>
2021-03-26 16:52:20 -0300

    feat(card component): creating the card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import styled from '@xstyled/styled-components'
+
+const Card = () => {
+  return <div>Teste</div>
+}
+
+export default Card
