34f14c951826b561af0528f40db6c17af66ea6ba
coelhomariana <coelhomariana@outlook.com>
2021-04-05 18:35:55 -0300

    feat(media card): styled the image, border, avatar and props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,61 +1,109 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useMemo } from 'react'
 import styled from '@xstyled/styled-components'
 
-import { Box, Flex, Avatar, Typography, Button } from '../'
+import { Box, Flex, Avatar, Typography, Heading, Paragraph, Button } from '../'
 
-const Card = ({ media, avatar, headerText, complementaryText, contentText }) => {
+const Card = ({
+  media,
+  avatar,
+  title,
+  headerPosition,
+  avatarPosition,
+  subtitle,
+  content,
+  children,
+  hasCallToAction
+}) => {
+  const isDefaultHeaderPosition = useMemo(() => headerPosition === 'row', [headerPosition])
+
   return (
     <CardContainer flexDirection='column' justifyContent='space-between'>
-      <MediaContainer>
-        <img
-          src='https://images.pexels.com/photos/772429/pexels-photo-772429.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-          width='280'
-        ></img>
-      </MediaContainer>
+      {!!media && (
+        <MediaContainer borderRadius='400'>
+          <img width='100%' height='auto' src={media}></img>
+          <AvatarContainer>
+            <Avatar>{avatar}</Avatar>
+          </AvatarContainer>
+        </MediaContainer>
+      )}
+
       <Box margin='24px'>
-        <Flex margin='16px'>
+        <Flex flexDirection={headerPosition} mb={isDefaultHeaderPosition ? 8 : 0}>
+          {!!avatar && (
+            <Box paddingRight={!!title || !!subtitle ? 24 : 0}>
+              <Avatar avatar={avatar} size='very-large'></Avatar>
+            </Box>
+          )}
+
           <Box>
-            <Avatar size='small'>{avatar}</Avatar>
-          </Box>
-          <Box margin='2px'>
-            <Flex>
-              <Typography font-weight='600'>{headerText}</Typography>
-            </Flex>
-            <Flex>
-              <Typography>{complementaryText}</Typography>
-            </Flex>
+            {!!title && (
+              <Box mt={!isDefaultHeaderPosition ? 16 : 0}>
+                <Heading variant='h4'>{title}</Heading>
+              </Box>
+            )}
+
+            {!!subtitle && (
+              <Paragraph variant='sm' fontWeight='400' color='#757575'>
+                {subtitle}
+              </Paragraph>
+            )}
           </Box>
         </Flex>
-        <Box mt='30px' mb='30px'>
-          <Typography>Content text.{contentText}</Typography>
-        </Box>
-        <Flex justifyContent='space-between'>
-          <Button variant='filled' caption='Primary' width='104px'></Button>
-          <Button variant='outlined' caption='Secondary' width='104px'></Button>
+
+        <Flex mt={!!title && !!subtitle && !!media && !!avatar ? 24 : 8}>
+          {!!content && <Typography>{content}</Typography>}
         </Flex>
+
+        {hasCallToAction && (
+          <Flex justifyContent='space-between' align-items='left'>
+            <Button variant='filled' caption='Primary' width='50%' mr='16px' mt='24px'></Button>
+            <Button variant='outlined' caption='Secondary' width='50%' ml='16px' mt='24px'></Button>
+          </Flex>
+        )}
+
+        {children}
       </Box>
     </CardContainer>
   )
 }
 
 const CardContainer = styled(Flex)`
-  width: 280px;
-  height: 480px;
+  width: 100%;
+  max-width: 280px;
+  height: auto;
   border-radius: 4px;
   box-shadow: 0px 2px 4px rgba(33, 33, 33, 0.2);
 `
 
 const MediaContainer = styled(Flex)`
-  max-width: 280px;
+  position: relative;
+
+  img {
+    border-top-right-radius: 4px;
+    border-top-left-radius: 4px;
+  }
 `
+
+const AvatarContainer = styled(Flex)`
+  position: absolute;
+  left: 24px;
+  top: calc(100% - 32px);
+`
+
+Card.defaultProps = {
+  headerPosition: 'row',
+  avatarPosition: 'default'
+}
 
 Card.propTypes = {
   media: PropTypes.string,
   avatar: PropTypes.string,
-  headerText: PropTypes.string,
-  complementaryText: PropTypes.string,
-  contentText: PropTypes.string
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  content: PropTypes.string,
+  children: PropTypes.string,
+  hasCallToAction: PropTypes.bool
 }
 
 export default Card
