8182e1d70f992744a5e86d5f9970cd6d4ef6e4ec
Samanta Tessmer <samanta.tessmer@nave.rs>
2021-08-16 11:57:47 -0300

    feat: replaced color

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
-import {variant, compose, color, layout, space, border } from 'styled-system'
+import { variant, compose, color, layout, space, border } from 'styled-system'
 import PropTypes from 'prop-types'
 import { Typography } from '../'
 import { Icon } from '../Iconography'
@@ -18,59 +18,63 @@ const Tag = ({ children, close, ...props }) => (
 
 const baseProps = compose(color, layout, space, border)
 
-const selectedVariant = ({theme: {colors}, color}) => variant({
-  default: true,
-  prop: 'selected',
-  key: 'tag',
-  variants: {
-    true: {
-      backgroundColor: colors[color],
-      borderColor: colors[color],
-      cursor: 'pointer',
-      p: {
-        color: 'white'
-      }
-    },
-    false: {
-      backgroundColor: 'transparent',
-      borderColor: colors.gray['600'],
-      cursor: 'pointer',
-      p: {
-        color: colors.gray['600']
-      }
-    },
-    disabled: {
-      backgroundColor: 'disabled',
-      borderColor: 'disabled',
-      cursor: 'default',
-      p: {
-        color: 'white'
+const selectedVariant = ({ theme: { colors }, color }) =>
+  variant({
+    default: true,
+    prop: 'selected',
+    key: 'tag',
+    variants: {
+      true: {
+        backgroundColor: colors[color],
+        borderColor: colors[color],
+        cursor: 'pointer',
+        p: {
+          color: 'white'
+        }
+      },
+      false: {
+        backgroundColor: 'transparent',
+        borderColor: colors.gray['600'],
+        cursor: 'pointer',
+        p: {
+          color: colors.gray['600']
+        }
+      },
+      disabled: {
+        backgroundColor: 'disabled',
+        borderColor: 'disabled',
+        cursor: 'default',
+        p: {
+          color: 'white'
+        }
       }
     }
-  }
-})
+  })
 
 const Base = styled.div(
   props => css`
-  display: inline-block;
-  border-radius: 1;
-  border-width: 1px;
-  border-style: solid;
-  ${border}
-  ${baseProps}
+    display: inline-block;
+    border-radius: ${props.theme.space[1]}px;
+    border-width: 1px;
+    border-style: solid;
+    ${border}
+    ${baseProps}
   ${selectedVariant(props)}
-`)
+  `
+)
 
 const Content = styled.div`
   display: flex;
   align-items: center;
 `
 
-const Text = styled(Typography)`
-  font-weight: 600;
-  font-size: 12px;
-  line-height: 16px;
-`
+const Text = styled(Typography)(
+  ({ theme: { fontWeights, lineHeights, fontSizes } }) => css`
+    font-weight: ${fontWeights[1]};
+    font-size: ${fontSizes[1]}px;
+    line-height: ${lineHeights[1]};
+  `
+)
 
 Tag.defaultProps = {
   selected: true,
