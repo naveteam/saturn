54e659b8773eb751c3bab8edda71dd4fd18fa97e
igorcasconi <igor492@gmail.com>
2021-08-29 17:48:13 -0300

    refactor(table): removed xstyled

--- src/components/Table/TableContent.jsx
+++ src/components/Table/TableContent.jsx
@@ -1,12 +1,13 @@
-import styled from '@xstyled/styled-components'
-import { th } from '@xstyled/system'
+import styled, { css } from 'styled-components'
 
-const TableContent = styled.tbody`
-  border-top: 2px solid ${th.color('gray.300')};
-  position: relative;
-  &:first-child {
-    border-top: none;
-  }
-`
+const TableContent = styled.tbody(
+  ({ theme: { colors } }) => css`
+    border-top: 2px solid ${colors.gray['300']};
+    position: relative;
+    &:first-child {
+      border-top: none;
+    }
+  `
+)
 
 export default TableContent
