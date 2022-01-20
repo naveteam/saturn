f1675cc2982103df285e0c6a72f54ce31c7b3da0
Yuri Farnesio <yuri.farnesio@gmail.com>
2022-01-20 17:13:32 -0300

    fix(radio-button): refactor props and some styles

--- src/components/RadioButton/RadioButton.jsx
+++ src/components/RadioButton/RadioButton.jsx
@@ -1,93 +1,103 @@
 import React, { forwardRef } from 'react'
-import styled from 'styled-components'
-import { typography } from 'styled-system'
+import styled, { css } from 'styled-components'
 import PropTypes from 'prop-types'
 
 import { Typography } from '../'
 import { Flex } from '../Grid'
 import { Icon } from '../Iconography'
 
-const RadioButton = forwardRef(({ color, label, disabled, checked, name, onChange, value, ...props }, ref) => {
-  return (
-    <LabelContainer as='label' color={color} minHeight={24} {...props}>
-      <Input
-        type='radio'
-        value={value}
-        ref={ref}
-        disabled={disabled}
-        checked={checked}
-        name={name}
-        onChange={onChange}
-        {...props}
-      />
-      <CheckedIcon icon='radio_button_checked' color='primary' />
-      <UncheckedIcon icon='radio_button_outline' />
-      {label && (
-        <Typography fontSize={3} lineHeight={3} fontWeight={0} paddingLeft={3}>
-          {label}
-        </Typography>
-      )}
-    </LabelContainer>
-  )
-})
+const RadioButton = forwardRef(
+  (
+    { name, label, value, disabled = false, onChange, defaultChecked = false, checked = false, color, id, ...props },
+    ref
+  ) => {
+    return (
+      <Flex alignItems='center' {...props}>
+        <RadioButtonContainer disabled={disabled}>
+          <HiddenInput
+            ref={ref}
+            type='radio'
+            name={name}
+            id={id}
+            value={value}
+            onChange={e => {
+              if (onChange) onChange(e)
+            }}
+            disabled={disabled}
+            defaultChecked={defaultChecked}
+            checked={checked ? checked : undefined}
+          />
+
+          <CheckedIcon icon='radio_button_checked' color={disabled ? 'disabled' : 'primary'} />
+          <UncheckedIcon icon='radio_button_outline' color={disabled ? 'disabled' : 'gray.800'} />
+          {label && (
+            <Typography
+              fontSize={3}
+              lineHeight={3}
+              fontWeight={0}
+              paddingLeft={3}
+              color={disabled ? 'disabled' : color ? color : 'gray.800'}
+            >
+              {label}
+            </Typography>
+          )}
+        </RadioButtonContainer>
+      </Flex>
+    )
+  }
+)
 
 const CheckedIcon = styled(Icon)`
-  display: none;
+  opacity: 0;
+  transition: opacity 0.2s linear;
 `
 const UncheckedIcon = styled(Icon)`
-  & path {
-    fill: gray.700;
-  }
+  opacity: 1;
+  position: absolute;
+  transition: opacity 0.2s linear;
 `
-const LabelContainer = styled(Flex)`
-  color: gray.800;
 
-  & > input:checked:enabled ~ p {
-    color: gray.900;
-    ${typography}
-  }
-  ${typography}
-`
-const Input = styled.input`
-  opacity: 0;
-  margin: 0;
-  height: 0;
-  width: 0;
-
-  & ~ p,
-  & ~ svg {
+const RadioButtonContainer = styled.label(
+  ({ disabled }) => css`
+    height: 24px;
+    display: flex;
+    align-items: center;
     cursor: pointer;
-  }
 
-  &:disabled ~ p,
-  &:disabled ~ svg {
-    cursor: not-allowed;
-  }
+    svg {
+      width: 20px;
+      height: 20px;
+    }
+
+    ${disabled
+      ? css`
+          cursor: not-allowed;
+        `
+      : ''}
+  `
+)
+const HiddenInput = styled.input`
+  display: none;
 
   &:checked + ${CheckedIcon} {
-    display: inherit;
+    opacity: 1;
+
     & + ${UncheckedIcon} {
-      display: none;
+      opacity: 0;
     }
-  }
-
-  &:disabled ~ svg path {
-    fill: gray.400;
-  }
-
-  &:disabled ~ p {
-    color: disabled;
   }
 `
 
 RadioButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool]),
+  disabled: PropTypes.bool,
+  defaultChecked: PropTypes.bool,
   checked: PropTypes.bool,
   color: PropTypes.string,
-  disabled: PropTypes.bool,
   onChange: PropTypes.func,
-  label: PropTypes.string,
-  name: PropTypes.string,
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool])
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 }
 
 export default RadioButton
